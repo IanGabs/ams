9afea5ade14d2562be7b928205a1bd9d5ff448c2
IanGabs <ianbielbia223@gmail.com>
2023-04-26 11:49:49 -0300

    aula

---
+++ app/Aula.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+Class Aula {
+    public function extrairInformacoes()
+    {
+        //Acessar o arquivo aula.js da aula e pegar a lista de questão, que é a propiedade "conteudo"
+        //Para cada questão, acessar a pasta em questão e no arquivo questao.js pegar o id
+        return "TESTE";
+    }
+}
--- index.php
+++ index.php
@@ -2,14 +2,16 @@
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
+use App\Aula;
 
 require __DIR__ . '/vendor/autoload.php';
 
 $app = AppFactory::create();
 
 $app->get('/', function (Request $request, Response $response, $args) {
-    $response->getBody()->write("Hello world!");
+    $aula = new Aula();
+    echo $aula->extrairInformacoes();
     return $response;
 });
-$app->setBasePath('/aula');
+$app->setBasePath('/ams/ams');
 $app->run();
